53143120029902dbc7c662d042c7beea755f55b5
raczman <devnull@localhost>
2009-11-12 17:56:21 +0100

    Build system can compile .idl files, example usage in realmd/CMakeLists.txt

--- cmake/FindTAO.cmake
+++ cmake/FindTAO.cmake
@@ -4,11 +4,14 @@ set(TAO_FIND_LIBS "PortableServer"  "CosNaming" "CosEvent" "CosEvent_Skel" "AnyT
 
 find_path(TAO_INCLUDE_DIR tao/corba.h PATHS ${ACE_INCLUDE_DIR} /usr/include /usr/local/include)
 find_library(TAO_LIBRARY NAMES TAO TAOd ${ACE_INCLUDE_DIR}/lib $ENV{TAO_ROOT}/lib /usr/lib /usr/local/lib)
+find_program(TAO_IDL_COMPILER "tao_idl" ${ACE_INCLUDE_DIR}/bin /usr/bin /usr/local/bin ${TAO_INCLUDE_DIR}/bin)
+message(STATUS "tao_idl at: ${TAO_IDL_COMPILER}")
 
-if(TAO_INCLUDE_DIR AND TAO_LIBRARY)
+if(TAO_INCLUDE_DIR AND TAO_LIBRARY AND TAO_IDL_COMPILER)
   SET(TAO_FOUND TRUE)
-endif(TAO_INCLUDE_DIR AND TAO_LIBRARY)
+endif(TAO_INCLUDE_DIR AND TAO_LIBRARY AND TAO_IDL_COMPILER)
 #now let's search for parts of tao we need.
+
 if(TAO_FOUND)
 
   foreach(LIBRARY ${TAO_FIND_LIBS})
