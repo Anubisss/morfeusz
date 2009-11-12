ba8d03d5b0af10ee23781677d4c8e778f04ce412
raczman <devnull@localhost>
2009-11-12 01:22:17 +0100

    Added FindTAO script for linux and windows. Tao is now compulsory to compile, as well as modules listed in cmake/FintTAO.cmake

---
+++ cmake/FindTAO.cmake
@@ -0,0 +1,26 @@
+
+set(TAO_FOUND FALSE)
+set(TAO_FIND_LIBS "PortableServer"  "CosNaming" "CosEvent" "CosEvent_Skel" "AnyTypeCode")
+
+find_path(TAO_INCLUDE_DIR tao/corba.h PATHS ${ACE_INCLUDE_DIR} /usr/include /usr/local/include)
+find_library(TAO_LIBRARY NAMES TAO TAOd ${ACE_INCLUDE_DIR}/lib $ENV{TAO_ROOT}/lib /usr/lib /usr/local/lib)
+
+if(TAO_INCLUDE_DIR AND TAO_LIBRARY)
+  SET(TAO_FOUND TRUE)
+endif(TAO_INCLUDE_DIR AND TAO_LIBRARY)
+#now let's search for parts of tao we need.
+if(TAO_FOUND)
+
+  foreach(LIBRARY ${TAO_FIND_LIBS})
+    find_library(TAO_${LIBRARY}_LIB NAMES "TAO_${LIBRARY}" "TAO_${LIBRARY}d" ${TAO_LIBRARY} ${ACE_ROOT}/lib /usr/lib /usr/local/lib)
+    if(TAO_${LIBRARY}_LIB)
+      message(STATUS "${LIBRARY} found at: ${TAO_${LIBRARY}_LIB}")
+      list(APPEND ${TAO_LIBRARY} TAO_${LIBRARY}_LIB)
+    else(TAO_${LIBRARY}_LIB)
+      SET(TAO_FOUND FALSE)
+    endif(TAO_${LIBRARY}_LIB)
+  endforeach()
+
+endif(TAO_FOUND)
+
+
