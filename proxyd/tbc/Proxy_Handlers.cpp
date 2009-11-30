ae6afb7ba9d68fccbb904b36f20d0fd4900fe7a6
raczman <devnull@localhost>
2009-11-30 22:11:32 +0100

    Send SMSG_AUTH_RESPONSE when client is done authenticating. Client now goes into retrieving characters phase.

--- proxyd/tbc/Proxy_Handlers.cpp
+++ proxyd/tbc/Proxy_Handlers.cpp
@@ -184,7 +184,13 @@ Proxy_Socket::account_retrieved(bool state)
 
   this->crypto->set_key(K);
 
-  
+  ServerPkt* pkt = new ServerPkt(SMSG_AUTH_RESPONSE, 1 + 4 + 1 + 4 + 1);
+  *pkt << (uint8)0x0C;  //AUTH_OK
+  *pkt << (uint32)0x00;
+  *pkt << (uint8)0x00;
+  *pkt << (uint32)0x00;
+  *pkt << (uint8)this->acct.expansion;
+  this->send(pkt);
 
   BN_free(K);
   BN_free(N);
